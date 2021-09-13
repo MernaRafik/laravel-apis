34fe4145f83ac7aedcabcaf49e51617e8239cad0
MernaRafik <48571363+MernaRafik@users.noreply.github.com>
2021-09-14 00:11:38 +0200

    Update api.php

--- blog-api/routes/api.php
+++ blog-api/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); */
+
+Route::get('user/{id}',[UserController::class, 'show_user']);
+Route::get('articles/{id}',[ArticleController::class, 'find_id']);
+Route::post('create/articles',[ArticleController::class, 'create']);
+Route::put('update/articles/{id}',[ArticleController::class, 'update']);
+Route::delete('delete/articles/{id}',[ArticleController::class, 'delete']);
+Route::get('user/{id}/articles',[ArticleController::class, 'article_user']);
+Route::post('articles/{id}/comments',[CommentController::class, 'create_comment']);
+Route::get('articles/{id}/comments',[CommentController::class, 'show_comment']);
